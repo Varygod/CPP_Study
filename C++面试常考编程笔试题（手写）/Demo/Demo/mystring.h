766055f8c7d78021340388e58c120c6896c9ba48
Varygod <pshenghua2006@163.com>
2019-07-23 00:31:35 +0800

    新增demo

--- C++面试常考编程笔试题（手写）/Demo/Demo/mystring.h
+++ C++面试常考编程笔试题（手写）/Demo/Demo/mystring.h
@@ -4,4 +4,6 @@
 #include <stddef.h>
 char * mystrcpy(char *strDest, const char *strSrc);
 void * mymemcpy(char *strDest, const char *strSrc, size_t count);
+int myatoi(char *str);
+char *myitoa(int num);
 #endif  //__MYSTRING_H__
--- C++面试常考编程笔试题（手写）/Demo/Demo/test.c
+++ C++面试常考编程笔试题（手写）/Demo/Demo/test.c
@@ -4,9 +4,15 @@
 
 int  main(int argc, char* argv[])
 {
-	char *str = "abcdefg";
-	char buf[1024] = { 0 };
-	mystrcpy(buf, str);
-	printf("buf = %s", buf);
+	//char *str = "abcdefg";
+	//char buf[1024] = { 0 };
+	//mystrcpy(buf, str);
+	//printf("buf = %s", buf);
+
+	char str[] = "0123456789";
+	//mymemcpy(str + 1,str + 0, 9);
+	//printf("%s\n", str);
+	mymemcpy(str, str + 5, 5);
+	printf("%s\n", str);
 	system("pause");
 }
