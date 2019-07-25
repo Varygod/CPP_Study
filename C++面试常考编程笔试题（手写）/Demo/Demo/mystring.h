e6eb78687436b92be977111b996c8d17ea92e717
Varygod <pshenghua2006@163.com>
2019-07-26 01:23:08 +0800

    新增demo

--- C++面试常考编程笔试题（手写）/Demo/Demo/mystring.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#ifndef __MYSTRING_H__
-#define __MYSTRING_H__
-#include <stddef.h>
-char * mystrcpy(char *strDest, const char *strSrc);
-void * mymemcpy(char *strDest, const char *strSrc, size_t count);
-int myatoi(char *str);
-char *myitoa(int num);
-#endif  //__MYSTRING_H__
